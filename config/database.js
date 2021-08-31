a75442a7d87946d2bcbdff4f066eb385bc74d0a5
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2021-08-31 22:21:11 +0700

    Refactor Config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,24 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env("NODE_ENV", "development"),
   connections: {
-    default: {
-      connector: 'bookshelf',
+    production: {
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: false,
+      },
+      options: {},
+    },
+    development: {
+      connector: "bookshelf",
+      settings: {
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         useNullAsDefault: true,
--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("BASE_URL", "http://localhost"),
+  proxy: env.bool("IS_PROXIED", false),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '4b63698bd93e9d0bdf32e9c661eb3083'),
+      secret: env("ADMIN_JWT_SECRET", "4b63698bd93e9d0bdf32e9c661eb3083"),
+      serveAdminPanel: env.bool("SERVE_ADMIN", true),
     },
   },
 });
